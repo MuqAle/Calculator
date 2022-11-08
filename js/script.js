443fdeff11a019a81ea49b371db2e39f43a0ee4f
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2022-11-08 12:20:05 -0500

    Added multiple buttons

--- js/script.js
+++ js/script.js
@@ -3,17 +3,48 @@ const subtractBtn = document.querySelector('#subtract');
 const multiplyBtn = document.querySelector('#multiply');
 const divideBtn = document.querySelector('#divide');
 const digitBtn = document.querySelectorAll('.digit');
+const output = document.querySelector("#output");
+const clearBtn = document.querySelector('#clear-btn');
+const operatorBtn = document.querySelectorAll('.operator');
+const decimalBtn = document.querySelector('#decimal');
+const add = (a,b) => {return a + b};
+const subtract = (a,b) => {return a - b};
+const multiply = (a,b) => {return a * b};
+const divide = (a,b) => {return a / b};
+
+
+let numbersStored = {};
 
 
 
 
+function inputDigits(){
+    for (let btn of digitBtn){
+        btn.addEventListener('click', (e) => {
+            output.textContent += `${e.target.textContent}`});
+    }
 
-const add = (a,b) => {return a + b}
-const subtract = (a,b) => {return a - b}
-const multiply = (a,b) => {return a * b}
-const divide = (a,b) => {return a / b}
+}
+
+
+function inputOperator(){
+    for (let btn of operatorBtn){
+        btn.addEventListener('click', (e) => {
+            numbersStored['first_number'] = output.textContent
+            numbersStored['operator'] = `${e.target.textContent}`
+            output.textContent = ''  
+        });
+    }
+
+}
+
+
+
+
 
 function operate(operator, a , b){
+    a = numbersStored['first_number']
+    operator = numbersStored['operator'];
     switch(operator){
         case '+':{
             return add(a ,b);
@@ -21,7 +52,7 @@ function operate(operator, a , b){
         case '-':{
             return subtract(a,b);
         }
-        case '*':{
+        case 'x':{
             return multiply(a,b);
         }
         case '/':{
@@ -29,3 +60,15 @@ function operate(operator, a , b){
         }
     }
 }
+
+clearBtn.addEventListener('click', () => {
+    output.textContent = '';
+    for (const key in numbersStored){
+        delete numbersStored[key];
+    }
+    decimalBtn.disabled = false;
+})
+
+decimalBtn.addEventListener('click', () => decimalBtn.disabled = true)
+inputDigits()
+inputOperator()
